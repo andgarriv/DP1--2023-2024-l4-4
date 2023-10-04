0d3e4452fa2ee5d086234a3b7b91c23485a9b22e
jormunrod <jormunrod@alum.us.es>
2023-10-04 11:02:00 +0200

    Fix error

--- frontend/src/util/deleteFromList.js
+++ frontend/src/util/deleteFromList.js
@@ -24,25 +24,14 @@ export default function deleteFromList(url, id, [state, setState], [alerts, setA
                     else
                         setState(state.filter((i) => i.id !== id));
                 }
-<<<<<<< HEAD
-                return response.json();
-            })
-            .then(json => {
-                getDeleteAlertsOrModal(json, id, alerts, setAlerts, setMessage, setVisible);
-=======
                 return response.text();
             })
             .then(text => {if(text!=='')
                         getDeleteAlertsOrModal(JSON.parse(text), id, alerts, setAlerts, setMessage, setVisible);
->>>>>>> upstream/main
             })
             .catch((err) => {
                 console.log(err);
                 alert("Error deleting entity")
             });
     }
-<<<<<<< HEAD
 }
-=======
-}
->>>>>>> upstream/main
