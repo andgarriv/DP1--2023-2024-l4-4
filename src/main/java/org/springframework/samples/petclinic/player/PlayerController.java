1309ce41d5017c2ffce9ebdc238bcda04aee1087
IsaacSolisPadilla <isasolpad@alum.us.es>
2023-11-09 18:28:08 +0100

    CreatePlayer Controller

--- src/main/java/org/springframework/samples/petclinic/player/PlayerController.java
+++ src/main/java/org/springframework/samples/petclinic/player/PlayerController.java
@@ -1,11 +1,16 @@
 package org.springframework.samples.petclinic.player;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.validation.Valid;
 
 @RestController
 @RequestMapping("api/v1/player")
@@ -19,6 +24,14 @@ public class PlayerController {
     public PlayerController(PlayerService playerService){
         this.playerService = playerService;
     }
+
+    @PostMapping()
+    @ResponseStatus(HttpStatus.CREATED)
+    public Player create(@RequestBody @Valid Player player){
+        return playerService.createPlayer(player);        
+    }
+
+
     
 }
 
