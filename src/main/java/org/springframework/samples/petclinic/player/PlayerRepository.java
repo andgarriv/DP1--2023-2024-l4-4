eca4026cb925c5ddd45226d57d98649f28fc8b21
Jorge Muñoz Rodríguez <jorgemr@protonmail.ch>
2023-11-12 18:15:15 +0100

    Fix player methots

--- src/main/java/org/springframework/samples/petclinic/player/PlayerRepository.java
+++ src/main/java/org/springframework/samples/petclinic/player/PlayerRepository.java
@@ -5,15 +5,14 @@ import java.util.Optional;
 
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
-
+@Repository
 public interface PlayerRepository extends CrudRepository<Player, Integer> {
     @Query("SELECT p FROM Player p")
     public List<Player> findAllPlayers();
 
-    /*@Query("SELECT p FROM Player p WHERE p.nickname = :nickname")
-    public Player findByNickname(String nickname);*/
-
+    @Query("SELECT p FROM Player p WHERE p.nickname = :nickname")
     Optional<Player> findByNickname(String nickname);
 
     @Query("DELETE FROM Player p WHERE p.nickname = :nickname")
@@ -22,7 +21,8 @@ public interface PlayerRepository extends CrudRepository<Player, Integer> {
     @Query("SELECT COUNT(*) > 0 FROM Player p WHERE p.nickname = ?1")
     public Boolean existsByNickname(String nickname);
 
-    Optional<Player> findById(Integer id);
+    @Query("SELECT p FROM Player p WHERE p.id = :id")
+    public Optional<Player> findById(Integer id);
 
     @Query("SELECT COUNT(*) > 0 FROM Player p WHERE p.email = ?1")
     public Boolean existsByEmail(String email);
