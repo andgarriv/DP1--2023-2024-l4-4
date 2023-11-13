476c4287efe256a31540eebe898b8c6386b291da
JavierRodriguez <javrodrei@alum.us.es>
2023-11-13 15:15:06 +0100

    H18 almost

--- src/main/java/us/l4_4/dp1/end_of_line/gameplayer/GamePlayerRepository.java
+++ src/main/java/us/l4_4/dp1/end_of_line/gameplayer/GamePlayerRepository.java
@@ -1,6 +1,11 @@
 package us.l4_4.dp1.end_of_line.gameplayer;
 
+import java.util.List;
+
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import us.l4_4.dp1.end_of_line.game.Game;
 
 @Repository
 public class GamePlayerRepository {
