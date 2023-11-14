a2fde7155b8068f31e52e4d8caa1a7fe228bdb2b
Jorge Muñoz Rodríguez <jorgemr@protonmail.ch>
2023-11-14 11:21:40 +0100

    Fixing register wip

--- src/main/java/us/l4_4/dp1/end_of_line/player/PlayerController.java
+++ src/main/java/us/l4_4/dp1/end_of_line/player/PlayerController.java
@@ -2,6 +2,8 @@ package us.l4_4.dp1.end_of_line.player;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.security.access.method.P;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -15,6 +17,8 @@ import org.springframework.web.bind.annotation.RestController;
 import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
+import us.l4_4.dp1.end_of_line.auth.AuthService;
+
 
 @RestController
 @RequestMapping("/api/v1/player")
@@ -23,16 +27,34 @@ import jakarta.validation.Valid;
 public class PlayerController {
 
     private PlayerService playerService;
+    private AuthService authService;
+    public final PasswordEncoder encoder;
 
     @Autowired
-    public PlayerController(PlayerService playerService){
+    public PlayerController(PlayerService playerService, AuthService authService, PasswordEncoder encoder) {
         this.playerService = playerService;
+        this.authService = authService;
+        this.encoder = encoder;
     }
+
 
     @PostMapping()
     @ResponseStatus(HttpStatus.CREATED)
     public Player create(@RequestBody @Valid Player player){
-        return playerService.createPlayer(player);        
+        String password2 = authService.encodePassword(player.getPassword());
+        player.setPassword(password2);
+        Player newPlayer = new Player();
+        newPlayer.setName(player.getName());
+        newPlayer.setSurname(player.getSurname());
+        newPlayer.setPassword(password2);
+        newPlayer.setEmail(player.getEmail());
+        newPlayer.setBirthDate(player.getBirthDate());
+        newPlayer.setNickname(player.getNickname());
+        newPlayer.setAvatar(player.getAvatar());
+        // authService.createUser2(player);
+        // return player;
+        playerService.createPlayer(player);   
+        return newPlayer;
     }
 
     @GetMapping("/{id}")
