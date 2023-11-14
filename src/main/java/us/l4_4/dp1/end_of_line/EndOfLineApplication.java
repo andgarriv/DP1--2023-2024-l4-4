e5a3db02c73e9338f70d8a5b1654970835997e9d
Angelgares <angelgares6424@gmail.com>
2023-11-14 01:03:34 +0100

    Minor change

--- src/main/java/us/l4_4/dp1/end_of_line/EndOfLineApplication.java
+++ src/main/java/us/l4_4/dp1/end_of_line/EndOfLineApplication.java
@@ -4,9 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication()
-public class PetclinicApplication {
+public class EndOfLineApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(PetclinicApplication.class, args);
+		SpringApplication.run(EndOfLineApplication.class, args);
 	}
 }
