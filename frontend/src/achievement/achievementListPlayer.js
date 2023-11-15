fff572dc783ed978fed17babc8f494440cd223ff
IsaacSolisPadilla <isasolpad@alum.us.es>
2023-11-15 01:17:32 +0100

    Porcentaje Achievement añadido

--- frontend/src/achievement/achievementListPlayer.js
+++ frontend/src/achievement/achievementListPlayer.js
@@ -8,6 +8,7 @@ export default function AchievementPlayer() {
     const jwt = tokenService.getLocalAccessToken();
     const [player, setPlayer] = useState(null); // Inicializa player como null
     const [doAchievement, setDoAchievement] = useState([]); // Inicializa doAchievement como un arreglo vacío
+    const [percentageCompleted, setPercentageCompleted] = useState(0);
 
     useEffect(() => {
         async function fetchData() {
@@ -39,6 +40,15 @@ export default function AchievementPlayer() {
         fetchData();
     }, [jwt, user.id]);
 
+    useEffect(() => {
+        if (player && doAchievement.length > 0) {
+            const completedAchievements = doAchievement.filter((achievement) => 
+                player.playerAchievement.find(a => a.achievement.id === achievement.id)?.achieveAt
+            ).length;
+            setPercentageCompleted(((completedAchievements / doAchievement.length) * 100).toFixed(0)); 
+        }
+    }, [player, doAchievement]);
+
     if (!player) {
         return <div className="loading">Loading player data...</div>;
     }
@@ -46,32 +56,45 @@ export default function AchievementPlayer() {
     return (
         <div className="home-page-container">
             <div className="hero-div">
-                <h1 style={{ textAlign: 'center' }}>Achievements</h1>
-                <div>
-                    {doAchievement && doAchievement.length > 0 ? (
-                        doAchievement.map((achievement) => (
-                            <div key={achievement.id} style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', marginBottom: '20px' }}>
-                                <img 
-                                    src={achievement.badgeImage || imgnotfound} 
-                                    alt={achievement.name} 
-                                    width="50px" 
-                                    style={{ marginRight: '10px' }}
-                                />
-                                <strong style={{ marginRight: '10px' }}>{achievement.name}</strong>
-                                <span style={{ marginRight: '10px' }}>{achievement.description}</span>
-                                <span>
-                                    {player.playerAchievement.find(a => a.achievement.id === achievement.id)?.achieveAt ? (
-                                        `${player.playerAchievement.find(a => a.achievement.id === achievement.id)?.achieveAt}`
-                                    ) : (
-                                        <span style={{ color: 'red' }}>Not completed</span>
-                                    )}
-                                </span>
-                            </div>
-                        ))
-                    ) : (
-                        <p>No achievements found.</p>
-                    )}
-                </div>
+                <h1 style={{ textAlign: 'center' }}>Achievements {percentageCompleted}%</h1>
+                
+                <table>
+                    <tbody>
+                        {doAchievement && doAchievement.length > 0 ? (
+                            doAchievement.map((achievement) => (
+                                <tr key={achievement.id}>
+                                    <td className="text-center">
+                                        <div style={{ marginRight: "40px", marginBottom: "15px" }}>
+                                            <img src={achievement.badgeImage || imgnotfound} alt={achievement.name} width="50px" />
+                                        </div>
+                                        
+                                    </td>
+                                    <td className="text-center" colSpan="2">
+                                        <div style={{ marginRight: "40px", marginBottom: "15px" }}>
+                                            <strong>{achievement.name}</strong>
+                                            <br />
+                                            <span style={{ fontSize: "13px" }}>{achievement.description}</span>
+                                        </div>
+                                    </td>
+                                    <td className="text-center">
+                                        <div style={{ marginRight: "40px", marginBottom: "15px" }}>
+                                                {player.playerAchievement.find(a => a.achievement.id === achievement.id)?.achieveAt ? (
+                                                    `${player.playerAchievement.find(a => a.achievement.id === achievement.id)?.achieveAt}`
+                                                ) : (
+                                                    <span style={{ color: 'red' }}>Not completed</span>
+                                                )}
+                                        </div>
+                                    </td>
+
+                                </tr>
+                            ))
+                        ) : (
+                            <tr>
+                                <td colSpan="4">No achievements found.</td>
+                            </tr>
+                        )}
+                    </tbody>
+                </table>
             </div>
         </div>
     );
