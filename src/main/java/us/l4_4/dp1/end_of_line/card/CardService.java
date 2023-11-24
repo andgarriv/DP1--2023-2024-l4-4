51a5f27ff31c7bb32d6bc8f3b703bc2f1fa1a02d
JavierRodriguez <javrodrei@alum.us.es>
2023-11-24 19:59:34 +0100

    CRUD Card

--- src/main/java/us/l4_4/dp1/end_of_line/card/CardService.java
+++ src/main/java/us/l4_4/dp1/end_of_line/card/CardService.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import jakarta.validation.Valid;
 import us.l4_4.dp1.end_of_line.enums.Color;
 
 @Service
@@ -31,6 +32,15 @@ public class CardService {
 
         return cardRepository.findAll();
     }
+
+    
+    @Transactional(readOnly = true)
+    public Card saveCard(@Valid Card c ){
+
+        return cardRepository.save(c);
+    }
+
+    
 
 
     
