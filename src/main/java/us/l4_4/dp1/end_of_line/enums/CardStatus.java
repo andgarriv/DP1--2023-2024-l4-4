de60d4a8de444b039409eb2225fca398917a6611
andgarriv <andres7212@gmail.com>
2023-11-27 19:35:05 +0100

    aaaa

--- src/main/java/us/l4_4/dp1/end_of_line/enums/CardStatus.java
+++ src/main/java/us/l4_4/dp1/end_of_line/enums/CardStatus.java
@@ -1,11 +1,7 @@
 package us.l4_4.dp1.end_of_line.enums;
 
 public enum CardStatus {
-<<<<<<< HEAD
     IN_HAND,
     IN_DECK,
     ON_BOARD
-=======
-    IN_HAND, IN_DECK, ON_BOARD
->>>>>>> origin/main
 }
--- src/main/java/us/l4_4/dp1/end_of_line/enums/Exit.java
+++ src/main/java/us/l4_4/dp1/end_of_line/enums/Exit.java
@@ -1,15 +1,6 @@
 package us.l4_4.dp1.end_of_line.enums;
 
 public enum Exit {
-<<<<<<< HEAD
-    EXIT_001,
-    EXIT_010,
-    EXIT_011,
-    EXIT_100,
-    EXIT_101,
-    EXIT_110,
-    EXIT_111
-=======
     EXIT_001_A, EXIT_001_B, EXIT_001_C, EXIT_001_D,
     EXIT_010_A, EXIT_010_B, EXIT_010_C, EXIT_010_D, EXIT_010_E, EXIT_010_F, EXIT_010_G, EXIT_010_H,
     EXIT_011_A, EXIT_011_B,
@@ -17,5 +8,4 @@ public enum Exit {
     EXIT_101_A, EXIT_101_B,
     EXIT_110_A, EXIT_110_B,
     EXIT_111_A, EXIT_111_B;
->>>>>>> origin/main
 }
