e3eca59d71d256ece0d42b7fb09dfb055d1549f3
andgarriv <andres7212@gmail.com>
2023-11-27 19:31:28 +0100

    Merge with main

---
+++ src/main/java/us/l4_4/dp1/end_of_line/enums/CardStatus.java
@@ -0,0 +1,7 @@
+package us.l4_4.dp1.end_of_line.enums;
+
+public enum CardStatus {
+    IN_HAND,
+    IN_DECK,
+    ON_BOARD
+}
--- src/main/java/us/l4_4/dp1/end_of_line/enums/Exit.java
+++ src/main/java/us/l4_4/dp1/end_of_line/enums/Exit.java
@@ -1,5 +1,11 @@
 package us.l4_4.dp1.end_of_line.enums;
 
 public enum Exit {
-    EXIT_001, EXIT_010, EXIT_100, EXIT_101, EXIT_110, EXIT_111
+    EXIT_001,
+    EXIT_010,
+    EXIT_011,
+    EXIT_100,
+    EXIT_101,
+    EXIT_110,
+    EXIT_111
 }
