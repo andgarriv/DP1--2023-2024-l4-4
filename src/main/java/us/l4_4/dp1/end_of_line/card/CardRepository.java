b500f2f85930a85402cca0641e6b58e9b74f6df0
IsaacSolisPadilla <isasolpad@alum.us.es>
2023-11-29 13:44:39 +0100

    Operacion con Cards

--- src/main/java/us/l4_4/dp1/end_of_line/card/CardRepository.java
+++ src/main/java/us/l4_4/dp1/end_of_line/card/CardRepository.java
@@ -2,11 +2,13 @@ package us.l4_4.dp1.end_of_line.card;
 
 import java.util.List;
 
+import org.springframework.boot.actuate.autoconfigure.health.HealthProperties.Status;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 import us.l4_4.dp1.end_of_line.enums.Color;
+import us.l4_4.dp1.end_of_line.game.Game;
 
 @Repository
 public interface CardRepository extends CrudRepository<Card, Integer>{
@@ -16,4 +18,7 @@ public interface CardRepository extends CrudRepository<Card, Integer>{
 
         @Query("SELECT c FROM Card c WHERE c.color = ?1 AND c.is_Template = true")
         List<Card> findTemplatedCardsByColor(Color color);
+
+         @Query("SELECT c FROM GamePlayer g JOIN g.cards c WHERE g.id = ?1")
+        List<Card> findCardsByStatus(Status status);
 }
