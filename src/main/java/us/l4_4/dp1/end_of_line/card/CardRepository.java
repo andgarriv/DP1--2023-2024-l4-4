5749843db54baae6737547ee52e74ed91d39cc5a
Jorge Muñoz Rodríguez <jorgemr@protonmail.ch>
2023-12-01 15:11:20 +0100

    Get cards by game updated

--- src/main/java/us/l4_4/dp1/end_of_line/card/CardRepository.java
+++ src/main/java/us/l4_4/dp1/end_of_line/card/CardRepository.java
@@ -9,6 +9,7 @@ import org.springframework.stereotype.Repository;
 
 import us.l4_4.dp1.end_of_line.enums.Color;
 import us.l4_4.dp1.end_of_line.game.Game;
+import us.l4_4.dp1.end_of_line.gameplayer.GamePlayer;
 
 @Repository
 public interface CardRepository extends CrudRepository<Card, Integer>{
@@ -21,4 +22,7 @@ public interface CardRepository extends CrudRepository<Card, Integer>{
 
         @Query("SELECT c FROM GamePlayer g JOIN g.cards c WHERE g.id = ?1")
         List<Card> findCardsByStatus(Status status);
+
+        @Query("SELECT c FROM GamePlayer g JOIN g.cards c WHERE g.id = ?1")
+        List<Card> findCardsByGamePlayer(Integer gamePlayerId);
 }
