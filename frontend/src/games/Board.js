d0e1781a57d85ac63402ecc0ee303698ef702560
andgarriv <andres7212@gmail.com>
2023-12-02 00:38:11 +0100

    Prueba in hand cards

--- frontend/src/games/Board.js
+++ frontend/src/games/Board.js
@@ -12,12 +12,17 @@ function Box({ content }) {
     );
 }
 
-function importGameCard(color, exit){
+function importGameCard(color, exit, iniciative) {
     let name = '';
-    if(exit === 'START'){
-        name = 'C'+color.toUpperCase()[0]+'_START';
+    let exitSubstring = exit.replace('EXIT_', '').substring(0, 3);
+    if (exit === 'START') {
+        name = 'C' + color.toUpperCase()[0] + '_START';
     } else {
-        name = 'C'+color.toUpperCase()[0]+'_'+exit.replace('EXIT_','').substring(0,2);
+        if (exitSubstring === '111') {
+            name = 'C' + color.toUpperCase()[0] + '_111_' + iniciative;
+        } else {
+            name = 'C' + color.toUpperCase()[0] + '_' + exitSubstring;
+        }
     }
     return import(`../static/images/GameCards/${name}.png`);
 }
@@ -31,7 +36,18 @@ export default function Board() {
         Array(7).fill(null).map(() => Array(7).fill(null))
     );
     const gameId = window.location.pathname.split("/")[window.location.pathname.split("/").length - 1];
-    
+    const [dataGamePlayer, setDataGamePlayer] = useState([]);
+    const [handCardsPlayer1, setHandCardsPlayer1] = useState([]);
+    const [handCardsPlayer2, setHandCardsPlayer2] = useState([]);
+    const [isLoading, setIsLoading] = useState(true);
+
+    async function setHandCardsPlayer(cards, setHandCardsFunction) {
+        const handCardImages = await Promise.all(cards.map(card =>
+            importGameCard(card.color, card.exit, card.iniciative).then(module => ({ ...card, image: module.default }))
+        ));
+        setHandCardsFunction(handCardImages);
+    }
+
 
     useEffect(() => {
         async function fetchGameCards() {
@@ -48,21 +64,32 @@ export default function Board() {
                         'Authorization': `Bearer ${jwt}`
                     }
                 });
+
                 const dataGamePlayer = await responseGamePlayer.json();
+                setDataGamePlayer(dataGamePlayer);
                 const data = await response.json();
-    
+                fetch(`/api/v1/games/fivecards/${dataGamePlayer[0].id}`, {
+                    headers: {
+                        'Authorization': `Bearer ${jwt}`
+                    }
+                });
+                fetch(`/api/v1/games/fivecards/${dataGamePlayer[1].id}`, {
+                    headers: {
+                        'Authorization': `Bearer ${jwt}`
+                    }
+                });
                 // Filtrar las cartas de inicio
                 const startCards = data.filter(card => card.exit === "START");
-    
+
                 // Cargar imágenes para las cartas de inicio
-                const startCardImages = await Promise.all(startCards.map(card => 
-                    importGameCard(card.color, card.exit).then(module => ({ ...card, image: module.default }))
+                const startCardImages = await Promise.all(startCards.map(card =>
+                    importGameCard(card.color, card.exit, card.iniciative).then(module => ({ ...card, image: module.default }))
                 ));
-    
+
                 // Actualizar el tablero con las cartas de inicio
                 setBoard(oldBoard => {
                     const newBoard = oldBoard.map(row => row.slice());
-    
+
                     startCardImages.forEach(card => {
                         // Asumiendo posiciones fijas para las cartas de inicio
                         if (card.color === dataGamePlayer[0].color) {
@@ -72,32 +99,63 @@ export default function Board() {
                             newBoard[card.row][card.column] = { image: card.image };
                         }
                     });
-    
+
                     return newBoard;
                 });
+
+                const handCardsPlayer1 = data.filter(card => card.color === dataGamePlayer[0].color && card.card_Status === "IN_HAND");
+                const handCardsPlayer2 = data.filter(card => card.color === dataGamePlayer[1].color && card.card_Status === "IN_HAND");
+
+                await setHandCardsPlayer(handCardsPlayer1, setHandCardsPlayer1);
+                await setHandCardsPlayer(handCardsPlayer2, setHandCardsPlayer2);
+
+                setIsLoading(false);
+
             } catch (error) {
                 console.error('Error al cargar las cartas', error);
             }
         }
         fetchGameCards();
     }, [gameId, jwt]);
-    
+
+    if (isLoading) {
+        return <div>Loading...</div>;
+    }
 
     return (
         <div className="background">
-            {/* ...otro contenido... */}
             <br />
-            <div className="board">
-                {board.map((row, i) => (
-                    <div key={i} className="row2">
-                        {row.map((boxContent, j) => (
-                            <Box
-                                key={j}
-                                content={boxContent}
-                            />
-                        ))}
+            <div style={{ display: 'flex', flexDirection: 'row' }}>
+                <div className="player-column">
+                    {dataGamePlayer.length > 0 ? dataGamePlayer[0].player.nickname : null}
+                    <br />
+                    {handCardsPlayer1.map((card, index) => (
+                    <div className= "hand" key={index}>
+                        <img src={card.image} alt="Card" />
                     </div>
                 ))}
+            </div>
+                <div className="board">
+                    {board.map((row, i) => (
+                        <div key={i} className="row2">
+                            {row.map((boxContent, j) => (
+                                <Box
+                                    key={j}
+                                    content={boxContent}
+                                />
+                            ))}
+                        </div>
+                    ))}
+                </div>
+                <div className="player-column">
+                    {dataGamePlayer.length > 0 ? dataGamePlayer[1].player.nickname : null}
+                    <br />
+                    {handCardsPlayer2.map((card, index) => (
+                    <div className= "hand" key={index}>
+                        <img src={card.image} alt="Card" />
+                    </div>
+                ))}
+                </div>
             </div>
         </div>
     );
