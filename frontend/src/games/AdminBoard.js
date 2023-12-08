d2f834682020ef92a4978fcdbaa3903ce13256b5
Jorge Muñoz Rodríguez <jorgemr@protonmail.ch>
2023-12-08 19:12:45 +0100

    Changed names of files in game frontend

--- frontend/src/games/AdminBoard.js
+++ frontend/src/games/AdminBoard.js
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from "react";
-import tokenService from "../services/token.service";
+import tokenService from "../services/token.service.js";
+import { fetchGameCards, isPlayerAuthorized } from "./services/boardService.js";
 import "./styles/Board.css";
 
 function Box({ content }) {
@@ -10,105 +11,10 @@ function Box({ content }) {
   );
 }
 
-async function fetchGameCards(gameId, jwt, setDataGamePlayer, setHandCardsPlayer1, setHandCardsPlayer2, setBoard, setIsLoading) {
-  try {
-    const response = await fetch(`/api/v1/cards/game/${gameId}`, {
-      method: "GET",
-      headers: {
-        Authorization: `Bearer ${jwt}`,
-        "Content-Type": "application/json",
-      },
-    });
-    const responseGamePlayer = await fetch(
-      `/api/v1/gameplayers/game/${gameId}`,
-      {
-        headers: {
-          Authorization: `Bearer ${jwt}`,
-        },
-      }
-    );
-    const dataGamePlayer = await responseGamePlayer.json();
-    setDataGamePlayer(dataGamePlayer);
-    const data = await response.json();
-    const handCardsPlayer1 = data.filter(
-      (card) =>
-        card.color === dataGamePlayer[0].color &&
-        card.card_Status === "IN_HAND"
-    );
-    const handCardsPlayer2 = data.filter(
-      (card) =>
-        card.color === dataGamePlayer[1].color &&
-        card.card_Status === "IN_HAND"
-    );
-
-    setHandCardsPlayer(handCardsPlayer1, setHandCardsPlayer1);
-    setHandCardsPlayer(handCardsPlayer2, setHandCardsPlayer2);
-
-    const cardsOnBoard = data.filter((card) => card.card_Status === "ON_BOARD");
-
-    const cardsOnBoardImages = await Promise.all(
-      cardsOnBoard.map((card) =>
-        importGameCard(card.color, card.exit, card.iniciative).then(
-          (module) => ({ ...card, image: module.default })
-        )
-      )
-    );
-
-    // Actualizar el tablero con las cartas en el tablero
-    setBoard((oldBoard) => {
-      const newBoard = oldBoard.map((row) => row.slice());
-
-      cardsOnBoardImages.forEach((card) => {
-        if (card.color === dataGamePlayer[0].color) {
-          newBoard[card.row][card.column] = { image: card.image };
-        }
-        if (card.color === dataGamePlayer[1].color) {
-          newBoard[card.row][card.column] = { image: card.image };
-        }
-      });
-
-      return newBoard;
-    });
-
-    setIsLoading(false);
-  } catch (error) {
-    console.error("Error al cargar las cartas", error);
-  }
-}
-
-function importGameCard(color, exit, iniciative) {
-  let name = "";
-  let exitSubstring = exit.replace("EXIT_", "").substring(0, 3);
-  if (exit === "START") {
-    name = "C" + color.toUpperCase()[0] + "_START";
-  } else {
-    if (exitSubstring === "111") {
-      name = "C" + color.toUpperCase()[0] + "_111_" + iniciative;
-    } else {
-      name = "C" + color.toUpperCase()[0] + "_" + exitSubstring;
-    }
-  }
-  return import(`../static/images/GameCards/${name}.png`);
-}
-
-function setHandCardsPlayer(cards, setHandCardsFunction) {
-  const handCardImages = cards.map((card) => {
-    const module = importGameCard(card.color, card.exit, card.iniciative);
-    return module.then((mod) => ({ ...card, image: mod.default }));
-  });
-
-  Promise.all(handCardImages).then((images) => {
-    setHandCardsFunction(images);
-  });
-}
-
-
 export default function Board() {
   const jwt = tokenService.getLocalAccessToken();
   const user = tokenService.getUser();
   const [isAuthorized, setIsAuthorized] = useState(false);
-  const [player, setPlayer] = useState(null);
-
   const [board, setBoard] = useState(
     Array(7)
       .fill(null)
@@ -124,15 +30,22 @@ export default function Board() {
   const [isLoading, setIsLoading] = useState(true);
 
   useEffect(() => {
+    if (dataGamePlayer.length > 0) {
+      setIsAuthorized(isPlayerAuthorized(user, dataGamePlayer));
+    }
     const interval = setInterval(() => {
       fetchGameCards(gameId, jwt, setDataGamePlayer, setHandCardsPlayer1, setHandCardsPlayer2, setBoard, setIsLoading);
     }, 1000); // Actualization every second
     return () => clearInterval(interval);
 
-  }, [gameId, jwt]);
+  }, [jwt, user]);
 
   if (isLoading) {
     return <div>Loading...</div>;
+  }
+
+  if (!isAuthorized) {
+    return <div>You are not authorized to see this game. Only for admins.</div>;
   }
 
   return (
--- frontend/src/games/Board.js
+++ frontend/src/games/Board.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-import tokenService from "../services/token.service";
+import tokenService from "../services/token.service.js";
 import { fetchGameCards, isPlayerAuthorized } from "./services/boardService.js";
 import "./styles/Board.css";
 
