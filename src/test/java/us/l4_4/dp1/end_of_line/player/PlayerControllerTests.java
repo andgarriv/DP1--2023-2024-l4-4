fcd2cee83408c2a4734390ff034d13188907c646
Angelgares <angelgares6424@gmail.com>
2023-12-27 21:05:19 +0100

    [test]: PlayerControllerTests optimized

--- src/test/java/us/l4_4/dp1/end_of_line/player/PlayerControllerTests.java
+++ src/test/java/us/l4_4/dp1/end_of_line/player/PlayerControllerTests.java
@@ -38,6 +38,7 @@ class PlayerControllerTests {
     private static final String BASE_URL = "/api/v1/players";
     private static final Integer TEST_PLAYER_ID = 2;
     private static final String TEST_PLAYER_NICKNAME = "playerNickname";
+    private static final String avatar = "https://cdn-icons-png.flaticon.com/512/147/147144.png";
     
     @SuppressWarnings("unused")
     @Autowired
@@ -61,13 +62,26 @@ class PlayerControllerTests {
     @Autowired
 	private ObjectMapper objectMapper;
 
-    private Player admin;
-    private Player player;
-    private Player player2;
-    private Player player3;
-    private Player newPlayer;
-    private Authorities authority;
-    private Authorities authority2;
+    private Player admin, player, player2, player3, newPlayer; 
+    private Authorities authority, authority2;
+
+    private Player createPlayer(Integer id, Boolean isAdmin) {
+        Player player = new Player();
+        player.setId(id);
+        player.setName("Name" + id);
+        player.setSurname("Surname" + id);
+        player.setPassword("Player" + id + "!");
+        player.setEmail("player" + id + "@gmail.com");
+        player.setBirthDate(LocalDate.of(1999, 01, 01));
+        player.setNickname("Nickname" + id);
+        if (isAdmin) {
+            player.setAuthority(authority);
+        } else {
+            player.setAuthority(authority2);
+        }
+        player.setAvatar(avatar);
+        return player;
+    }
 
     @BeforeEach
     void setup() {
@@ -75,55 +89,14 @@ class PlayerControllerTests {
         authority.setId(1);
         authority.setAuthority("ADMIN");
 
-        LocalDate birthDate = LocalDate.of(1999, 01, 01);
-		String avatar = "https://cdn-icons-png.flaticon.com/512/147/147144.png";
-
-        admin = new Player();
-        admin.setId(1);
-        admin.setName("adminName");
-        admin.setSurname("adminSurname");
-        admin.setPassword("Adm1n!");
-        admin.setEmail("admin@gmail.com");
-        admin.setBirthDate(birthDate);
-        admin.setNickname("adminNickname");
-        admin.setAuthority(authority);
-
         authority2 = new Authorities();
         authority.setId(2);
         authority.setAuthority("PLAYER");
 
-        player = new Player();
-        player.setId(2);
-        player.setName("playerName");
-        player.setSurname("playerSurname");
-        player.setPassword("Play3r!");
-        player.setEmail("player@gmail.com");
-        player.setBirthDate(birthDate);
-        player.setNickname("playerNickname");
-        player.setAuthority(authority2);
-        player.setAvatar(avatar);
-
-        player2 = new Player();
-        player2.setId(3);
-        player2.setName("player2Name");
-        player2.setSurname("player2Surname");
-        player2.setPassword("PLay3r!");
-        player2.setEmail("player2@gmail.com");
-        player2.setBirthDate(birthDate);
-        player2.setNickname("player2Nickname");
-        player2.setAuthority(authority2);
-        player2.setAvatar(avatar);
-
-        player3 = new Player();
-        player3.setId(4);
-        player3.setName("player3Name");
-        player3.setSurname("player3Surname");
-        player3.setPassword("PLay3r!");
-        player3.setEmail("player3@gmail.com");
-        player3.setBirthDate(birthDate);
-        player3.setNickname("player3Nickname");
-        player3.setAuthority(authority2);
-        player3.setAvatar(avatar);
+        admin = createPlayer(1, true);
+        player = createPlayer(2, false);
+        player2 = createPlayer(3, false);
+        player3 = createPlayer(4, false);
 
         Friendship friendship = new Friendship();
         friendship.setId(1);
@@ -139,63 +112,55 @@ class PlayerControllerTests {
     }
 
     @Test
-    @WithMockUser(username = "admin1", password = "Adm1n!")
+    @WithMockUser(username = "Name1", password = "Player1!")
     void adminShouldFindAllPlayers() throws Exception {
-        when(this.playerService.findAll()).thenReturn(List.of(player, player2));
+        when(this.playerService.findAll()).thenReturn(List.of(player, player2, player3));
 
-        mockMvc.perform(get(BASE_URL + "/all")).andExpect(status().isOk()).andExpect(jsonPath("$.size()").value(2))
-                .andExpect(jsonPath("$[?(@.id == 2)].name").value("playerName"))
-                .andExpect(jsonPath("$[?(@.id == 3)].name").value("player2Name"));
+        mockMvc.perform(get(BASE_URL + "/all")).andExpect(status().isOk()).andExpect(jsonPath("$.size()").value(3))
+                .andExpect(jsonPath("$[?(@.id == 2)].name").value("Name2"))
+                .andExpect(jsonPath("$[?(@.id == 3)].name").value("Name3"))
+                .andExpect(jsonPath("$[?(@.id == 4)].name").value("Name4"));
     }
 
     @Test
-    @WithMockUser(username = "playerName", password = "Own3r!")
+    @WithMockUser(username = "Name2", password = "Player2!")
     void playerShouldFindByNickname() throws Exception {
-        when(this.playerService.findByNickname(TEST_PLAYER_NICKNAME)).thenReturn(player2);
+        when(this.playerService.findByNickname(TEST_PLAYER_NICKNAME)).thenReturn(player);
 
         mockMvc.perform(get(BASE_URL + "/nickname/{nickname}", TEST_PLAYER_NICKNAME)).andExpect(status().isOk())
-                .andExpect(jsonPath("$.name").value("player2Name"));
+                .andExpect(jsonPath("$.name").value("Name2"));
     }
 
     @Test
-    @WithMockUser(username = "playerName", password = "Own3r!")
+    @WithMockUser(username = "Name2", password = "Player2!")
     void playerShouldFindById() throws Exception {
         when(this.playerService.findById(TEST_PLAYER_ID)).thenReturn(player);
 
         mockMvc.perform(get(BASE_URL + "/{id}", TEST_PLAYER_ID)).andExpect(status().isOk())
-                .andExpect(jsonPath("$.name").value("playerName"));
+                .andExpect(jsonPath("$.name").value("Name2"));
     }
 
     @Test
-    @WithMockUser(username = "playerName", password = "Own3r!")
+    @WithMockUser(username = "Name2", password = "Player2!")
     void playerShouldFindAllFriendsByPlayerId() throws Exception{
         when(this.playerService.findAllFriendsByPlayerId(TEST_PLAYER_ID)).thenReturn(List.of(player2));
 
         mockMvc.perform(get(BASE_URL + "/{id}/friends", TEST_PLAYER_ID)).andExpect(status().isOk())
                 .andExpect(jsonPath("$.size()").value(1))
-                .andExpect(jsonPath("$[?(@.id == 3)].name").value("player2Name"));
+                .andExpect(jsonPath("$[?(@.id == 3)].name").value("Name3"));
     }
 
     @Test
     @WithMockUser()
     void shouldCreatePlayer() throws Exception {
-        newPlayer = new Player();
-        newPlayer.setId(4);
-        newPlayer.setName("newPlayerName");
-        newPlayer.setSurname("newPlayerSurname");
-        newPlayer.setPassword("Play3r!");
-        newPlayer.setEmail("newplayer@gmail.com");
-        newPlayer.setBirthDate(LocalDate.of(1999, 01, 01));
-        newPlayer.setNickname("newplayerNickname");
-        newPlayer.setAuthority(authority2);
-        newPlayer.setAvatar("https://cdn-icons-png.flaticon.com/512/147/147144.png");
+        newPlayer = createPlayer(5, false);
 
         mockMvc.perform(post(BASE_URL).with(csrf()).contentType(MediaType.APPLICATION_JSON)
-				.content(objectMapper.writeValueAsString(player))).andExpect(status().isCreated());
+				.content(objectMapper.writeValueAsString(newPlayer))).andExpect(status().isCreated());
     }
 
     @Test
-	@WithMockUser(username = "playerName", password = "Own3r!")
+	@WithMockUser(username = "Name2", password = "Player2!")
 	void shouldUpdatePlayer() throws Exception {
 		player.setName("UPDATED");
 		player.setSurname("UPDATED");
@@ -210,7 +175,7 @@ class PlayerControllerTests {
 	}
 
     @Test
-	@WithMockUser(username = "admin1", password = "Adm1n!")
+	@WithMockUser(username = "Name1", password = "Player1!")
 	  void shouldDeletePlayer() throws Exception {
 		when(this.playerService.findById(TEST_PLAYER_ID)).thenReturn(player);
 		
