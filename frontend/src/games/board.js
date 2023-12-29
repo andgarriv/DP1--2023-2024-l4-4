bdbe3a28fbf213c5a28e422c0748085bd49ad525
andgarriv <andres7212@gmail.com>
2023-12-29 11:52:12 +0100

    Board work in progress

--- frontend/src/games/board.js
+++ frontend/src/games/board.js
@@ -51,11 +51,46 @@ export default function Board() {
   const [isLoading, setIsLoading] = useState(true);
   const [player1CardPossiblePositions, setPlayer1CardPossiblePositions] = useState([]);
   const [player2CardPossiblePositions, setPlayer2CardPossiblePositions] = useState([]);
-  
+  const [selectedCard, setSelectedCard] = useState(null);
+  const [selectedPosition, setSelectedPosition] = useState(null);
+
+
   const handleBoxClick = (rowIndex, colIndex) => {
     console.log(`Casilla clickeada: fila ${rowIndex}, columna ${colIndex}`);
-    // TODO: Implementar lógica de click en casilla
+  
+    // Verifica si hay una carta seleccionada y una posición seleccionada
+    if (selectedCard && selectedPosition) {
+      // Verifica si la posición seleccionada coincide con una de las CardPossiblePositions
+      const isPlayer1 = dataGamePlayer[0].player.id === user.id;
+      const isPlayer2 = dataGamePlayer[1].player.id === user.id;
+  
+      const isValidPosition =
+        (isPlayer1 &&
+          player1CardPossiblePositions.some(
+            (pos) => pos.row === selectedPosition.row && pos.col === selectedPosition.col
+          )) ||
+        (isPlayer2 &&
+          player2CardPossiblePositions.some(
+            (pos) => pos.row === selectedPosition.row && pos.col === selectedPosition.col
+          ));
+  
+      if (isValidPosition) {
+        // Coloca la carta en la posición seleccionada en el tablero
+        const newBoard = [...board];
+        newBoard[selectedPosition.row][selectedPosition.col] = selectedCard;
+        setBoard(newBoard);
+  
+        // Limpia la carta seleccionada y la posición seleccionada
+        setSelectedCard(null);
+        setSelectedPosition(null);
+      }
+    } else {
+      // Si no hay carta seleccionada, registra la posición como seleccionada
+      setSelectedPosition({ row: rowIndex, col: colIndex });
+    }
   };
+  
+  
 
   useEffect(() => {
     if (dataGamePlayer.length > 0) {
@@ -84,42 +119,66 @@ export default function Board() {
           In Hand
           <br />
           {/* SHOW PLAYER 1 CARDS IF PLAYER 1 */}
-          {dataGamePlayer[0].player.id == user.id && 
-          handCardsPlayer1.map((card, index) => (
-            <div className="hand" key={index}>
+          {dataGamePlayer[0].player.id === user.id &&
+            handCardsPlayer1.map((card, index) => (
+              <div
+              className={`hand ${selectedCard === card ? 'selected' : ''}`}
+              key={index}
+              onClick={() => {
+                if (selectedCard === card) {
+                  // Deselecciona la carta si ya está seleccionada
+                  setSelectedCard(null);
+                } else {
+                  // Selecciona la carta si no está seleccionada
+                  setSelectedCard(card);
+                }
+              }}
+            >
               <img src={card.image} alt="Card" />
             </div>
           ))}
           {dataGamePlayer[0].player.id == user.id &&
-          <div className="hand">
-            <img
-            src={energyCards[0].image}
-            alt="EnergyCard0"
-            style={{
-              ...getRotationStyle(dataGamePlayer.length > 0 ? dataGamePlayer[0].energy : 0),
-              marginTop: '40px'
-            }}
-          />
-          </div>
+            <div className="hand">
+              <img
+                src={energyCards[0].image}
+                alt="EnergyCard0"
+                style={{
+                  ...getRotationStyle(dataGamePlayer.length > 0 ? dataGamePlayer[0].energy : 0),
+                  marginTop: '40px'
+                }}
+              />
+            </div>
           }
           {/* SHOW PLAYER 2 CARDS IF PLAYER 2 */}
-          {dataGamePlayer[1].player.id == user.id && 
-          handCardsPlayer2.map((card, index) => (
-            <div className="hand" key={index}>
+          {dataGamePlayer[1].player.id == user.id &&
+            handCardsPlayer2.map((card, index) => (
+              <div
+              className={`hand ${selectedCard === card ? 'selected' : ''}`}
+              key={index}
+              onClick={() => {
+                if (selectedCard === card) {
+                  // Deselecciona la carta si ya está seleccionada
+                  setSelectedCard(null);
+                } else {
+                  // Selecciona la carta si no está seleccionada
+                  setSelectedCard(card);
+                }
+              }}
+            >
               <img src={card.image} alt="Card" />
             </div>
           ))}
           {dataGamePlayer[1].player.id == user.id &&
-          <div className="hand">
-            <img
-            src={energyCards[1].image}
-            alt="EnergyCard0"
-            style={{
-              ...getRotationStyle(dataGamePlayer.length > 0 ? dataGamePlayer[1].energy : 0),
-              marginTop: '40px'
-            }}
-          />
-          </div>
+            <div className="hand">
+              <img
+                src={energyCards[1].image}
+                alt="EnergyCard0"
+                style={{
+                  ...getRotationStyle(dataGamePlayer.length > 0 ? dataGamePlayer[1].energy : 0),
+                  marginTop: '40px'
+                }}
+              />
+            </div>
           }
         </div>
         {/* SHOW BOARD */}
@@ -127,22 +186,22 @@ export default function Board() {
           {board.map((row, i) => (
             <div key={i} className="row2">
               {row.map((boxContent, j) => (
-                <Box 
-                key={j} 
-                content={boxContent} 
-                onClick={() => handleBoxClick(i, j)}
-                // Caso de ser jugador 1
-                isHighlighted = {(player1CardPossiblePositions.some(pos => pos.row === j && pos.col === i) && dataGamePlayer[0].player.id === user.id) 
-                                  || 
-                                 (player2CardPossiblePositions.some(pos => pos.row === j && pos.col === i) && dataGamePlayer[1].player.id === user.id)}
-                
+                <Box
+                  key={j}
+                  content={boxContent}
+                  onClick={() => handleBoxClick(i, j)}
+                  // Caso de ser jugador 1
+                  isHighlighted={(player1CardPossiblePositions.some(pos => pos.row === j && pos.col === i) && dataGamePlayer[0].player.id === user.id)
+                    ||
+                    (player2CardPossiblePositions.some(pos => pos.row === j && pos.col === i) && dataGamePlayer[1].player.id === user.id)}
+
                 />
               ))}
             </div>
           ))}
         </div>
         <div className="player-column">
-          
+
         </div>
       </div>
     </div>
