3c55dfeb8b954940e3b1b08a5655943d53cdb742
JavierRodriguez <javrodrei@alum.us.es>
2024-01-08 20:57:51 +0100

    [test] : cardServiceTest

--- src/test/java/us/l4_4/dp1/end_of_line/card/cardServiceTests.java
+++ src/test/java/us/l4_4/dp1/end_of_line/card/cardServiceTests.java
@@ -2,14 +2,17 @@ package us.l4_4.dp1.end_of_line.card;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.util.Date;
 import java.util.List;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import us.l4_4.dp1.end_of_line.enums.CardStatus;
 import us.l4_4.dp1.end_of_line.enums.Color;
 import us.l4_4.dp1.end_of_line.enums.Exit;
+import us.l4_4.dp1.end_of_line.enums.Orientation;
 import us.l4_4.dp1.end_of_line.exceptions.ResourceNotFoundException;
 
 
@@ -43,23 +46,57 @@ public class cardServiceTests {
         assertThrows(ResourceNotFoundException.class, () -> cardService.findAllCardsOfGame(1000));
     }
 
-    /* @Test
-    @Transactional
+
+    @Test
     void shouldDeleteCard(){
-        Integer initialCount = cardService.findAllCardsOfGame(17).size();
         Card card = new Card();
-        card.setId(500);
         card.setInitiative(1);
         card.setColor(Color.YELLOW);
         card.setExit(Exit.EXIT_001_A);
         card.setIsTemplate(false);
         card.setOrientation(Orientation.S);
-        cardService.save(card);
-        Integer count = cardService.findAllCardsOfGame(17).size();
-        cardService.delete(500);
-        Integer finalCount = cardService.findAllCardsOfGame(17).size();
-        assertEquals(initialCount, finalCount);
-        assertEquals(initialCount, 50);
-        assertEquals(count, finalCount + 1);
-    } */
+        Card cartaCreada = cardService.save(card);
+        cardService.delete(cartaCreada.getId());
+        assertThrows(ResourceNotFoundException.class, () -> cardService.findById(cartaCreada.getId()));
+     } 
+
+    @Test
+    void shouldCreateCard(){
+        Card card = new Card();
+        Date date = Date.from(java.time.Instant.now());
+        card.setInitiative(1);
+        card.setColor(Color.YELLOW);
+        card.setExit(Exit.EXIT_001_A);
+        card.setIsTemplate(false);
+        card.setOrientation(Orientation.S);
+        card.setCardState(CardStatus.IN_HAND);
+        Card cartaCreada = cardService.save(card);
+
+        assertEquals(1,cartaCreada.getInitiative());
+        assertEquals(Color.YELLOW,cartaCreada.getColor());
+        assertEquals(Exit.EXIT_001_A,cartaCreada.getExit());
+        assertEquals(false,cartaCreada.getIsTemplate());
+        assertEquals(Orientation.S,cartaCreada.getOrientation());
+        assertEquals(CardStatus.IN_HAND, cartaCreada.getCardState());
+    }
+    @Test
+    void shouldUpdateCard(){
+        
+        Card card = new Card();
+        card.setInitiative(1);
+        card.setColor(Color.YELLOW);
+        card.setExit(Exit.EXIT_001_A);
+        card.setIsTemplate(false);
+        card.setOrientation(Orientation.S);
+        Card cartaCreada = cardService.save(card);
+        CardDTO dto = new CardDTO();
+        dto.setColumn(4);
+        dto.setRow(3);
+        dto.setOrientation("N");
+
+        Card cartaActualizada = cardService.update(cartaCreada.getId(), dto);
+        assertEquals(3, cartaActualizada.getRow());
+        assertEquals(4, cartaActualizada.getColumn());
+        assertEquals(Orientation.N, cartaActualizada.getOrientation());
+    }
 }
